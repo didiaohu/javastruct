567680e840c1d122d6c59a43d83c5f5aeef8eb15
bob <301085344@qq.com>
2018-04-20 14:54:52 +0800

    提交~~

--- src/tree/BST.java
+++ src/tree/BST.java
@@ -108,14 +108,16 @@ public class BST<Key extends Comparable<Key>, Value> {
         if      (cmp < 0) x.left  = delete(x.left,  key);
         else if (cmp > 0) x.right = delete(x.right, key);
         else { 
-            if (x.right == null) return x.left;
-            if (x.left  == null) return x.right;
+            if (x.right == null) return x.left;  // 一个或无子结点情况
+            if (x.left  == null) return x.right;  // 一个或无子结点情况
+            
+            // 两个子结点情况
             Node t = x;
             x = min(t.right);
             x.right = deleteMin(t.right);
             x.left = t.left;
         } 
-        x.size = size(x.left) + size(x.right) + 1;
+        x.size = size(x.left) + size(x.right) + 1; // 更新结点计数器
         return x;
     }
 
