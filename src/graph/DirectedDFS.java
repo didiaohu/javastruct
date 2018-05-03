2876b0a333d6db21dd64b48ffa9ea18e98f3ffe0
bob <301085344@qq.com>
2018-05-03 14:24:39 +0800

    提交代码~~

--- src/graph/DirectedDFS.java
+++ src/graph/DirectedDFS.java
@@ -6,10 +6,12 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class DirectedDFS {
 	private boolean[] marked;
+	//单点可达性
 	public DirectedDFS(Digraph G, int s) {
 		marked = new boolean[G.V()];
 		dfs(G, s);
 	}
+	//多点可达性
 	public DirectedDFS(Digraph G, Iterable<Integer> sources) {
 		marked = new boolean[G.V()];
 		for(int s : sources )
@@ -24,17 +26,19 @@ public class DirectedDFS {
 		return marked[v];
 	}
 	public static void main(String[] args) {
-		Digraph G = new Digraph(new In(args[0]));
+		Digraph G = new Digraph(new In());
 		
 		Bag<Integer> sources = new Bag<Integer>();
+		int sourcesData[] = {1,3};  
+		for(int i = 0; i < sourcesData.length; i++)
+			sources.add(sourcesData[i]);
 		
-		for(int i = 1; i < args.length; i++)
-			sources.add(Integer.parseInt(args[i]));
-		
+		// 测试多点可达性
 		DirectedDFS reachable = new DirectedDFS(G, sources);
 		
+		StdOut.println("可达所有顶点为：");
 		for(int v = 0; v<G.V(); v++)
 			if(reachable.marked(v)) StdOut.print(v + "  ");
-		StdOut.println();
+		
 	}
 }
