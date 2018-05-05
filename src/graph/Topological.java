ebe0966783f87670e958d38b0beede0e61523e88
bob <301085344@qq.com>
2018-05-06 01:41:30 +0800

    提交~~

--- src/graph/Topological.java
+++ src/graph/Topological.java
@@ -1,12 +1,15 @@
 package graph;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Topological {
-	private Iterable<Integer> order;
+	private Iterable<Integer> order;    // 顶点的拓扑排序
 	public Topological(Digraph G) {
-		DirectedCycle cyclefinder = new DirectedCycle(G);
-		if(!cyclefinder.hasCycle()) {
-			DepthFirstOrder dfs = new DepthFirstOrder(G);
-			order = dfs.reversePost();
+		DirectedCycle cyclefinder = new DirectedCycle(G); 
+		if(!cyclefinder.hasCycle()) {   // 判断图是否有无环
+			DepthFirstOrder dfs = new DepthFirstOrder(G);   
+			order = dfs.reversePost();  // 逆后序
 		}
 	}
 	public Iterable<Integer> order(){
@@ -14,5 +17,12 @@ public class Topological {
 	}
 	public boolean isDAG() {
 		return order != null;
+	}
+	public static void main(String[] args) {
+		Digraph G = new Digraph(new In());   // 以图 3 为例
+		Topological top = new Topological(G);
+		StdOut.println("拓扑排序：");
+		for(int v : top.order())
+			StdOut.print(v+" ");
 	}
 }
