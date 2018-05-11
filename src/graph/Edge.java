8b53df3861619898ec0d27c56906f1618d11f596
bob <301085344@qq.com>
2018-05-11 20:19:55 +0800

    提交~~

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -10,18 +10,18 @@ public class Edge implements Comparable<Edge>{
 		this.w = w;
 		this.weight = weight;
 	}
-	public double weight(){
+	public double weight(){  
 		return weight;
 	}
-	public int either(){
+	public int either(){  // 边两端的顶点之一
 		return v;
 	}
-	public int other(int vertex){
+	public int other(int vertex){  // 另一个顶点
 		if (vertex == v) return w;
 		else if(vertex == w) return v;
 		else throw new RuntimeException("Inconsistent edge");
 	}
-	public int compareTo(Edge that){
+	public int compareTo(Edge that){  // 将这条边 e与that比较 
 		if(this.weight() < that.weight()) return -1;
 		else if(this.weight() > that.weight()) return +1;
 		else return 0;
--- src/graph/EdgeWeightedGraph.java
+++ src/graph/EdgeWeightedGraph.java
@@ -5,17 +5,18 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class EdgeWeightedGraph {
+	private static final String NEWLINE = System.getProperty("line.separator"); // 换行展示
 	private final int V;     // 顶点总数
 	private int E;           // 边的总数
 	private Bag<Edge>[] adj; // 邻接表
-	public EdgeWeightedGraph(int V){
+	public EdgeWeightedGraph(int V){  // 创建一幅含V个顶点空图
 		this.V = V;
 		this.E = 0;
 		adj = (Bag<Edge>[]) new Bag[V];
 		for(int v = 0; v < V; v++)
 			adj[v] = new Bag<Edge>();
 	}
-	public EdgeWeightedGraph(In in){
+	public EdgeWeightedGraph(In in){  // 从输入流中读取图
 		this(in.readInt());
         int E = in.readInt();
         for (int i = 0; i < E; i++) {
@@ -26,26 +27,25 @@ public class EdgeWeightedGraph {
             addEdge(e);
         }
 	}
-	public int V(){ return V; }
+	public int V(){ return V; }  
 	public int E(){ return E; }
-	public void addEdge(Edge e){
+	public void addEdge(Edge e){   // 向图中添加一条边 e
 		int v = e.either(), w = e.other(v);
 		adj[v].add(e);
 		adj[w].add(e);
 		E++;
 	}
-	public Iterable<Edge> adj(int v){
+	public Iterable<Edge> adj(int v){  // 和v相关联的所有边
 		return adj[v];
 	}
-	public Iterable<Edge> edges(){  // 返回加权无向图所有边
+	public Iterable<Edge> edges(){  // 图的所有边
 		Bag<Edge> list = new Bag<Edge>();
         for (int v = 0; v < V; v++) {
             int selfLoops = 0;
             for (Edge e : adj(v)) {
-                if (e.other(v) > v) {
+                if (e.other(v) > v) {  
                     list.add(e);
                 }
-                // add only one copy of each self loop (self loops will be consecutive)
                 else if (e.other(v) == v) {
                     if (selfLoops % 2 == 0) list.add(e);
                     selfLoops++;
@@ -54,20 +54,21 @@ public class EdgeWeightedGraph {
         }
         return list;
 	}
-	public String toString() {
+	public String toString() {   // 对象的字符串表示
         StringBuilder s = new StringBuilder();
-        s.append(V + " " + E);
+        s.append("顶点：" + V + " 边：" + E + NEWLINE);
         for (int v = 0; v < V; v++) {
             s.append(v + ": ");
             for (Edge e : adj[v]) {
                 s.append(e + "  ");
             }
+            s.append(NEWLINE);
         }
         return s.toString();
     }
 	public static void main(String[] args) {
 		EdgeWeightedGraph G = new EdgeWeightedGraph(new In());
+		StdOut.println("-------------------------");
         StdOut.println(G);
-
 	}
 }
