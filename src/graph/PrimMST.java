e24961ded970b83b98fc2f4f5b410bfdedab6b71
bob <301085344@qq.com>
2018-05-14 02:29:50 +0800

    提交

--- src/graph/PrimMST.java
+++ src/graph/PrimMST.java
@@ -1,7 +1,9 @@
 package graph;
 
 import base.Queue;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
 public class PrimMST {
 	private Edge[] edgeTo;    // 距离树最近的边
@@ -14,28 +16,27 @@ public class PrimMST {
 		distTo = new double[G.V()];
 		marked = new boolean[G.V()];
 		for(int v = 0; v < G.V(); v++)
-			distTo[v] = Double.POSITIVE_INFINITY;
+			distTo[v] = Double.POSITIVE_INFINITY;   // 初始化正无穷大
 		pq = new IndexMinPQ<Double>(G.V());
 		distTo[0] = 0.0;
-		pq.insert(0, 0.0);   // 用顶点 0 和权重 0 初始化 pq
+		pq.insert(0, 0.0);   // 从起始点 0 开始
 		while(!pq.isEmpty())
-			visit(G, pq.delMin());   // 将最近的顶点添加到树中
+			visit(G, pq.delMin());  
 	}
 	private void visit(EdgeWeightedGraph G, int v){ // 将顶点 v 添加到树中，更新数据
-		marked[v] = true;
+		marked[v] = true;     
 		for(Edge e : G.adj(v)){
 			int w = e.other(v);
-			
-			if(marked[w]) continue;  // v-w 失效
-			if(e.weight() < distTo[w]){  // 连接 w 和树的最佳边 Edge 变为 e
+			if(marked[w]) continue;  
+			if(e.weight() < distTo[w]){  // 权值最小的边
 				edgeTo[w] = e;
 				distTo[w] = e.weight();
-				if(pq.contains(w)) pq.change(w, distTo[w]);
-				else  pq.insert(w, distTo[w]);
+				if(pq.contains(w)) pq.change(w, distTo[w]);  // 替换操作
+				else  pq.insert(w, distTo[w]);               // 入队操作
 			}
 		}
 	}
-	public Iterable<Edge> edges(){
+	public Iterable<Edge> edges(){        // 获取最小生成树的所有边  
 		Queue<Edge> mst = new Queue<Edge>();
         for (int v = 0; v < edgeTo.length; v++) {
             Edge e = edgeTo[v];
@@ -45,10 +46,20 @@ public class PrimMST {
         }
         return mst;
 	}
-	public double weight() {
+	public double weight() {     // 最小生成树权重总值
 		double weight = 0.0;
         for (Edge e : edges())
             weight += e.weight();
         return weight;
+    }
+    public static void main(String[] args) {
+        EdgeWeightedGraph G = new EdgeWeightedGraph(new In());
+        PrimMST mst = new PrimMST(G);
+        StdOut.println("-------------------------");
+        StdOut.println("最小生成树：");
+        for (Edge e : mst.edges()) {
+            StdOut.println(e);
+        }
+        StdOut.print("最小生成树权重总值：" + mst.weight());
     }
 }
