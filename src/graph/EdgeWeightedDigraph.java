3d7b4c3994bdc7504cabc8c3fc42b8f919a09dde
bob <301085344@qq.com>
2018-05-17 09:50:53 +0800

    提交~~

---
+++ src/graph/EdgeWeightedDigraph.java
@@ -0,0 +1,36 @@
+package graph;
+
+import base.Bag;
+
+public class EdgeWeightedDigraph {
+	private final int V;
+	private int E;
+	private Bag<DirectedEdge>[] adj;
+	public EdgeWeightedDigraph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<DirectedEdge>[]) new Bag[V];
+		for(int v = 0; v < V; v++)
+			adj[v] = new Bag<DirectedEdge>();
+	}
+	public int V() {
+		return V;
+	}
+	public int E() {
+		return E;
+	}
+	public void addEdge(DirectedEdge e) {
+		adj[e.from()].add(e);
+		E++;
+	}
+//	public Iterator<Edge> adj(int v){
+//		return adj[v];
+//	}
+	public Iterable<DirectedEdge> edges(){
+		Bag<DirectedEdge> bag = new Bag<DirectedEdge>();
+		for(int v = 0; v < V; v++)
+			for(DirectedEdge e : adj[v])
+				bag.add(e);
+		return bag;
+	}
+}
